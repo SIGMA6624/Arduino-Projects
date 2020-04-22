7c31c84ed5c7164fb94f3823aa19b5e35ff26d99
janoah-policarpio <36905005+janoah-policarpio@users.noreply.github.com>
2020-04-22 16:16:37 +0800

    Add files via upload
    Simple project to enhance my nightlight that shows orange stars all over the room. Adds the functionality of turning on my nightlight when the light's off.

---
+++ DIY Night Light/Final/Final.ino
@@ -0,0 +1,40 @@
+
+
+int reading;
+
+unsigned long currentMillis;        
+unsigned long previousMillis = 0;        
+
+const long interval = 100;           
+
+int test = A0;
+int led = 12;
+
+int ldr[5];
+
+int j = 0;
+
+void setup(){
+  Serial.begin(9600);
+  
+  
+  pinMode(test,INPUT);
+  pinMode(led, OUTPUT);
+  
+  
+  for(int i = 0; i<5; i++){
+    ldr[i]=0;
+  }
+}
+
+void loop(){
+  reading = analogRead(test);
+  if(reading >= 950){
+    Serial.println(reading);
+    digitalWrite(led,HIGH);
+  }
+  else{
+    Serial.println(reading);
+    digitalWrite(led,LOW);
+  }
+}
